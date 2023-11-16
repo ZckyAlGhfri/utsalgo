90285ce3c1d3c24afe2690dc96e919fcccfe820a
Zacky Alghifari <146928937+ZckyAlGhfri@users.noreply.github.com>
2023-11-16 11:20:15 +0700

    Add files via upload

---
+++ Aritmatika.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package alpro;
+
+/**
+ *
+ * @author ZckyAlGhfri
+ */
+public class Aritmatika { 
+public static void main(String[] args) { 
+    int x = 4; 
+    int y = 2; 
+    int z = 3; 
+        System.out.println("Nilai x = " + x); 
+        System.out.println("Nilai y = " + y); 
+        System.out.print("x + y = "); 
+        System.out.println(x + y); 
+        System.out.print("x - y = "); 
+        System.out.println(x - y); 
+        System.out.print("x * y = "); 
+        System.out.println(x * y); 
+        System.out.print("x / y = "); 
+        System.out.println(x / y); 
+        System.out.print("x mod y = "); 
+        System.out.println(x % y); 
+        System.out.print("x mod z = "); 
+        System.out.println(x % z); 
+    } 
+} 
+
